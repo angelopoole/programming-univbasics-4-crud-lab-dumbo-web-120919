de5bc9a900a00e05001a67be78064435a4fa978b
Angelo Poole <angelo.poole@outlook.com>
2019-12-04 18:05:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = Array.new("Bark", "Ai", "nevermore", "Cinderwill")
+  my_array = Array.new(4,"Bark", "Ai", "nevermore", "Cinderwill")
 end
 
 def add_element_to_end_of_array(array, element)
